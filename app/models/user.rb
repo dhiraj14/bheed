bd30db5abe730f50964003955b8b078834c1a565
Dhiraj Mishra <dhiraj.mishra@weboniselab.com>
2016-09-01 18:00:59 +0530

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :omniauthable
+
+  has_many :identities
+
+  def twitter
+    identities.where(provider: 'twitter').first
+  end
+
+  def twitter_client
+    @twitter_client ||= Twitter.client(access_token: twitter.accesstoken)
+  end
+
+  def facebook
+    identities.where(provider: 'facebook').first
+  end
+
+  def facebook_client
+    @facebook_client ||= Facebook.client(access_token: facebook.accesstoken)
+  end
+end
